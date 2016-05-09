f6cb3eb0d0c7ffaf82d591502a6b08da20c6dea4
Дмитрий Жур <tehzienergy@gmail.com>
2016-05-09 20:04:37 +0300

    avatar v1.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
     $('.tile').css('height', $('.tile').width());
   });
   
+//  $('.tile').addClass('tile--active');
+  
   window.setInterval(function(){
     var random = Math.floor(Math.random()*10);
     $(".tile").eq(random).toggleClass('tile--active');
-  }, 3000);
+  }, 2500);
   
 });
