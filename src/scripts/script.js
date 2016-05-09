1c26e7d8165c75673b4a7c7d3741ffb374cd9768
Дмитрий Жур <tehzienergy@gmail.com>
2016-05-09 16:34:03 +0300

    tiles v0.5

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,4 +6,9 @@ $(document).ready(function() {
     $('.tile').css('height', $('.tile').width());
   });
   
+  window.setInterval(function(){
+    var random = Math.floor(Math.random()*10);
+    $(".tile").eq(random).toggleClass('tile--active');
+  }, 3000);
+  
 });
